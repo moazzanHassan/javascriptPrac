e6b83436e689a3b66071479dfc7fb2eff66d0c9e
Moazzan Hassan <moazzanhassan907@gmail.com>
2024-12-21 18:31:41 +0500

    for loop

---
+++ for_loop.js
@@ -0,0 +1,12 @@
+for(var i = 10;i>=0; i = i-1){
+    // console.log(i)
+}
+for(var i = 0 ;i <  10; i++){
+    // console.log(i)
+}
+
+var colors = ["red","green","blue"];
+
+for(var j = 0;j<colors.length;j++){
+    console.log(colors[j] + "\n")
+}
