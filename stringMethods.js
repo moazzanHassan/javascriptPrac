0d24dbd9e407f7a0e18f84f7fc022ef3d5ad0789
Moazzan Hassan <moazzanhassan907@gmail.com>
2024-12-23 00:59:46 +0500

    js prac

--- stringMethods.js
+++ stringMethods.js
@@ -12,4 +12,30 @@ var theWord = myString.substr(location,6)
 var change = theWord.toUpperCase()
 
 var newMyStr = myString.replace("really",change)
-console.log(newMyStr)
+// console.log(newMyStr)
+
+var food = ["chicken","radish","carrot","spanich"];
+for(var i = 0;i < food.length;i++){
+
+    // console.log(`${myString} ${food[i]}`)
+}
+
+for(var store of food){
+    // console.log(`${myString} ${store}`)
+
+}
+
+for(var i = 0; i < food.length;i++){
+    // var reminder = i % 2;
+    //check if the incrementor is even 
+    if(i%2 == 0){  
+        // console.log(i.toUpperCase)
+    console.log(`${myString} ${food[i].toUpperCase()}`)
+
+    }
+    else{
+        // console.log(i)
+    console.log(`${myString} ${food[i]}`)
+
+    }
+}
